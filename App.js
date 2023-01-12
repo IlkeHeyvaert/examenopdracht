b7034220c9411517e91aacf3b4ef9b6cf2207e68
IlkeHeyvaert <86114403+IlkeHeyvaert@users.noreply.github.com>
2023-01-12 15:17:49 +0100

    stack screens

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Searchbar } from 'react-native-paper';
+/*import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';*/
+
+import FavoritesScreen from './screens/FavoritesScreen.js';
+import ProfileScreen from './screens/ProfileScreen.js';
+import MusicScreen from './screens/MusicScreen.js'; 
+import DetailsScreen from './screens/DetailsScreen.js';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+		
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="Music" component={(MusicScreen)} /> 
+       <Stack.Screen name="Detail" component={(DetailsScreen)} /> 
+       <Stack.Screen name="Profile" component={(ProfileScreen)} />
+       <Stack.Screen name="Favorites" component={(FavoritesScreen)} />  
+   </Stack.Navigator> 
+   </NavigationContainer> 
+   
+   ); 
+  } 
+  
